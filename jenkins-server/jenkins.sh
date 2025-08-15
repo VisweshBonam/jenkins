5d9a67d63bce039ee7de1d4b1389e3ed24637698
Visweshbonam <viswesh.bonam@apoorva.com>
2025-08-15 18:48:40 +0530

    jenkins server

--- jenkins-server/jenkins.sh
+++ jenkins-server/jenkins.sh
@@ -1,3 +1,14 @@
+# Resize the root and var logical volumes and file systems
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
+
+# Resize the file systems
+xfs_growfs /
+xfs_growfs /var
+
+
+# jenkins installation
 sudo curl -o /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
